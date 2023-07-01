b286b533c2aa8c4c2d224d4347a702c5ee5a0e5e
devsai9 <saisiddhish@gmail.com>
2023-06-30 20:39:35 -0400

    Merged all room.html related JS files

--- js/room.js
+++ js/room.js
@@ -1,14 +1,108 @@
-import { db, doc, setDoc, orderedMsgsList, roomId, availableRooms, allowedEmails } from '/js/firestore.js';
-import { username, email, profile_picture } from '/js/auth.js';
+//* VARIABLES
+import { app } from '/js/init.js';
+import { getFirestore, collection, getDoc, getDocs, doc, setDoc, query, onSnapshot, orderBy, serverTimestamp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-const chatWindow = document.querySelector('#chat-window');
+const profilePictureBtn = document.querySelector('#profile-picture');
+const profileDropdownDetailsWrapper = document.querySelector('#profile-dropdown-details-wrapper');
+const profileDropwdownPicture = document.querySelector('#profile-dropwdown-picture');
+const profileDropdownName = document.querySelector('#profile-dropdown-name');
+const profileDropdownEmail = document.querySelector('#profile-dropdown-email');
 const input = document.querySelector('#input');
 const send = document.querySelector('#send');
+const chatWindow = document.querySelector('#chat-window');
 send.addEventListener('click', sendMsg);
+
+//* Firebase Authentication
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+// Initialize the Google Auth Provider
+const provider = new GoogleAuthProvider();
+let username;
+let profile_picture;
+let email;
+
+//* Sign in
+// Sign in with Google
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        username = user.displayName;
+        profile_picture = user.photoURL;
+        email = user.email;
+        signedIn();
+    } else {
+        notSignedIn();
+        window.location.href = 'index.html';
+    }
+});
+
+function signedIn() {
+    profilePictureBtn.src = profile_picture;
+    send.removeAttribute('disabled', 'true');
+    input.removeAttribute('disabled', 'true');
+    input.style.color = 'black';
+    input.value = '';
+}
+
+function notSignedIn() {
+    profilePictureBtn.src = '/images/default-pfp.jpg';
+    send.setAttribute('disabled', 'true');
+    input.setAttribute('disabled', 'true');
+    input.style.color = 'white';
+    input.value = 'Please Sign-in';
+}
+
+const searchParams = new URLSearchParams(window.location.search);
+const roomId = searchParams.get('roomId');
+
+//* Firebase Firestore
+// Initialize Firebase Firestore
+const db = getFirestore(app);
+
+// Define arrays used later
+let rooms = [];
+let roomIds = [];
+let msgsList = [];
+let orderedMsgsList = [];
+let availableRooms = [];
+let allowedEmails = [];
+
+const roomMessages = collection(db, 'rooms', roomId, "messages");
+const querySnapshot = await getDocs(collection(db, "rooms"));
+querySnapshot.forEach((doc) => {
+    rooms.push(doc.data());
+    roomIds.push(doc.data().roomId);
+    availableRooms.push(doc.id);
+    allowedEmails.push(doc.data().allowedEmails);
+});
 
 let index = availableRooms.indexOf(roomId);
 if (allowedEmails[index].includes(email) == false) {
     window.location.href = 'index.html';
+}
+
+let roomIdIndex = roomIds.indexOf(roomId);
+document.querySelector('#title').innerText = rooms[roomIdIndex].roomName + " " + rooms[roomIdIndex].roomEmoji;
+
+//* Live Chat
+const q = query(roomMessages);
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    msgsList = [];
+    querySnapshot.forEach((doc) => {
+        msgsList.push(doc.data());
+    });
+    orderData();
+});
+
+// Orders data and put into orderedMsgsList[]
+async function orderData() {
+    orderedMsgsList = [];
+    const orderDataq = query(roomMessages, orderBy('order'));
+    const querySnapshot3 = await getDocs(orderDataq);
+    querySnapshot3.forEach((doc) => {
+        orderedMsgsList.push(doc.data());
+    });
+    redrawChatWindow();
 }
 
 // Creates Firebase document
@@ -29,8 +123,6 @@ async function sendMsg() {
 
         let dateTime = (currentDate.getMonth()+1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear() + " at " + currentHour + ":" + currentMinutes + currentAM;
 
-        redrawChatWindow();
-
         let docOrder;
         try {
             docOrder = parseInt(orderedMsgsList[orderedMsgsList.length - 1].order);
@@ -43,7 +135,7 @@ async function sendMsg() {
             senderEmail: email,
             content: value,
             order: docOrder + 1,
-            timestamp: dateTime
+            timestamp: dateTime // serverTimestamp()
         });
 
         input.value = '';
@@ -105,5 +197,3 @@ document.onkeyup = function(eventKeyName) {
         send.click();
     }
 }
-
-export { redrawChatWindow, addToChatWindow };
